34b7a0ade02f59ff2bf22d850cb9483fbe697f33
cristovoxdgm <cristovoxdgm@gmail.com>
2019-11-09 16:54:15 -0300

    ok

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  final DocumentSnapshot snapshot;
+
+  const CategoryScreen({Key key, this.snapshot}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
   final DocumentSnapshot docSnap;
@@ -9,7 +10,12 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () {},
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => CategoryScreen(
+                  snapshot: docSnap,
+                )));
+      },
       title: Text(docSnap.data['title']),
       trailing: Icon(Icons.keyboard_arrow_right),
       leading: CircleAvatar(
