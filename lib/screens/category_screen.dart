783707cf9ffb9fc9a3a385b2b48ec858c41e7281
cristovoxdgm <cristovoxdgm@gmail.com>
2019-11-14 17:47:21 -0300

    Mais um estudo concluido, gridview, listview e tiles criadas, conexão com o firebase funcionando

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/datas/product_data.dart';
+import 'package:loja_virtual_app/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -8,6 +10,65 @@ class CategoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              labelPadding: EdgeInsets.all(10),
+              indicatorColor: Colors.white,
+              tabs: <Widget>[Icon(Icons.grid_on), Icon(Icons.list)],
+            ),
+            title: Text(snapshot.data['title']),
+            centerTitle: true,
+          ),
+          body: FutureBuilder<QuerySnapshot>(
+            future: Firestore.instance
+                .collection("products")
+                .document(snapshot.documentID)
+                .collection('items')
+                .getDocuments(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                return TabBarView(
+                  physics: NeverScrollableScrollPhysics(),
+                  children: <Widget>[
+                    GridView.builder(
+                      padding: EdgeInsets.all(4),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 4,
+                          mainAxisSpacing: 4,
+                          childAspectRatio: 0.65),
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                          type: "grid",
+                          data: ProductData.fromDocument(
+                              snapshot.data.documents[index]),
+                        );
+                      },
+                    ),
+                    ListView.builder(
+                      padding: EdgeInsets.all(4),
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                          type: "grid",
+                          data: ProductData.fromDocument(
+                              snapshot.data.documents[index]),
+                        );
+                      },
+                    )
+                  ],
+                );
+              }
+            },
+          )),
+    );
   }
 }
---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  final String type;
+  final ProductData data;
+
+  const ProductTile({Key key, this.type, this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
